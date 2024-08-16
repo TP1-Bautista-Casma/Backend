1879dd376e7a1ab0bc921b1e842794ef28fb9f29
JoyceBautista <u201924504@upc.edu.pe>
2024-08-15 23:03:48 -0500

    changes for deploy v.2

---
+++ api/serializers.py
@@ -0,0 +1,45 @@
+# serializers.py 
+from rest_framework import serializers
+from .models import User, ColorblindImage
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ['id','email', 'password']
+        
+
+    def create(self, validated_data):
+        user = User(
+            #username=validated_data['username'],
+            email=validated_data['email'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+
+class ColorblindImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ColorblindImage
+        fields = ['id', 'user', 'image', 'type', 'subtype', 'uploaded_at']
+        read_only_fields = ['user', 'uploaded_at']
+        
+
+class UserEditSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=False)
+
+    class Meta:
+        model = User
+        fields = ['email', 'password']
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        instance = super().update(instance, validated_data)
+        
+        if password:
+            instance.set_password(password)
+            instance.save()
+        
+        return instance
