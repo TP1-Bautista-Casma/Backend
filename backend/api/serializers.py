5e3d0c68bc095ef7afbc1fd3f35948f8108b13b4
JoyceBautista <u201924504@upc.edu.pe>
2024-08-25 16:59:37 -0500

    add configurations

--- backend/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import ColorblindImage
-
-class ColorblindImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ColorblindImage
-        fields = ['id', 'image', 'type', 'subtype', 'uploaded_at']
-        read_only_fields = ['uploaded_at']
--- backend/api/urls.py
+++
@@ -1,9 +0,0 @@
-""" from django.urls import path, include
-from rest_framework import routers
-from api import views
-
-router=routers.DefaultRouter()
-router.register('api', views.register, 'api')
-urlpatterns=[
-    path("api/v1",include(router.urls))
-] """
