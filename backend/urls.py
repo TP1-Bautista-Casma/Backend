b622e72de734b8b14573cdc1fbd225c6a5f56d45
JoyceBautista <u201924504@upc.edu.pe>
2024-09-01 11:30:36 -0500

    Add configurations

--- backend/urls.py
+++ backend/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from api import views
 from django.urls import path
 from api.views import upload_image
@@ -28,3 +28,5 @@ urlpatterns = [
     path('admin/', admin.site.urls), 
     path('upload/', upload_image, name='upload_image'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns +=staticfiles_urlpatterns()
